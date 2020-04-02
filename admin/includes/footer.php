d551aa8f335096bdefa5b4bcb99e15cc7437e763
Bhupendra <49445266+BhupendraA10@users.noreply.github.com>
2020-04-02 15:54:01 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+   <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; (Bhupendra,Simran,Gaganpreet Kaur)'s PHP Project</span>
+          </div>
+        </div>
+      </footer>
